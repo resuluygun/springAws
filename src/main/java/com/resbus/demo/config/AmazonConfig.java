14354647fa60e021f51ba24250dcf6941563c94e
resul <resuluygunn@gmail.com>
2022-07-31 21:21:20 +0300

    add s3 connect configs - missing

--- src/main/java/com/resbus/demo/config/AmazonConfig.java
+++ src/main/java/com/resbus/demo/config/AmazonConfig.java
@@ -1,2 +1,25 @@
-package com.resbus.demo.config;public class AmazonConfig {
+package com.resbus.demo.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Bean  // with this annotation Spring instantiate this method, then we can inject it
+    public AmazonS3 s3(){
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                System.getProperty("awsAccessKey"),
+                System.getProperty("awsAccessKey")
+        );
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials( new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
 }
--- src/main/java/com/resbus/demo/config/PropertyFileReader.java
+++ src/main/java/com/resbus/demo/config/PropertyFileReader.java
@@ -1,2 +1,40 @@
-package com.resbus.demo.config;public class PropertyFileReader {
+package com.resbus.demo.config;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Properties;
+
+public class PropertyFileReader {
+    public void getPropValues() throws IOException {
+        InputStream inputStream = null;
+        try {
+            Properties prop = new Properties();
+            String propFileName = "config.properties";
+
+            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+            }
+
+            Date time = new Date(System.currentTimeMillis());
+
+            String prop1 = prop.getProperty("awsAccessKey");
+            String prop2 = prop.getProperty("awsSecretKey");
+
+
+            System.setProperty("awsAccessKey",prop1);
+            System.setProperty("awsSecretKey",prop2);
+
+            System.out.println( "\n Properties Successfully Loaded On " + time);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            if (inputStream != null)  inputStream.close();
+        }
+    }
 }
