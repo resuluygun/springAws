10a37256399efb985eb995ee3d095149ab5b3774
resuluygun <resuluygunn@gmail.com>
2022-08-26 21:45:36 +0300

    create upload methods on both controller and service layer

--- src/main/java/com/resbus/demo/profile/UserProfileService.java
+++ src/main/java/com/resbus/demo/profile/UserProfileService.java
@@ -1,8 +1,10 @@
 package com.resbus.demo.profile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -19,4 +21,6 @@ public class UserProfileService {
     }
 
 
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+    }
 }
