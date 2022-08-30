6bda74de64cd069ef94b3137f9e9e5fa506aeb30
resuluygun <resuluygunn@gmail.com>
2022-08-30 23:07:26 +0300

    get users from backend and show them on frontend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import  {useState, useEffect} from "react";
+import axios from "axios";
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const fecthUserProfiles = async () =>{
+    const data = await axios.get("http://localhost:8080/api/v1/user-profile")
+    setUsers(data.data)
+    console.log(data);
+  }
+
+  useEffect( () => {
+    fecthUserProfiles();
+    console.log("users: ", users);
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> hello</h1>
+      {users.map((user) =>{
+        return <>
+        <h1>{user.username}</h1>
+        <p>{user.userProfileId}</p>
+        <h1>{user.userProfileImageLink}</h1>
+        </>
+      })}
     </div>
   );
 }
