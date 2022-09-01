c6a9512a299c0a9cb0dadcf4461c08988e725ee7
resuluygun <resuluygunn@gmail.com>
2022-09-01 23:26:38 +0300

    upload file to s3

--- src/main/java/com/resbus/demo/profile/UserProfileService.java
+++ src/main/java/com/resbus/demo/profile/UserProfileService.java
@@ -1,19 +1,26 @@
 package com.resbus.demo.profile;
+import com.resbus.demo.bucket.BucketName;
+import com.resbus.demo.filestore.FileStore;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
+
+import static org.apache.http.entity.ContentType.*;
 
 @Service
 public class UserProfileService {
 
     private final  UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
      List<UserProfile> getUserProfiles(){
@@ -24,6 +31,36 @@ public class UserProfileService {
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
         System.out.println(userProfileId);
         System.out.println(file);
+        if (file.isEmpty()) {
+            throw new IllegalStateException("Cannot upload empty file");
+        }
+        else if (!Arrays.asList(IMAGE_JPEG.getMimeType(), IMAGE_PNG.getMimeType(), IMAGE_GIF.getMimeType())
+                .contains(file.getContentType())){
+            throw new IllegalStateException("File must be an image");
+        }
+
+        UserProfile user =getUserProfiles()
+                .stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findAny()
+                .orElse(null) ;
+        if (user== null){
+            throw new IllegalStateException(String.format("User profile %s not found",userProfileId));
+        }
+
+        Map<String, String> metadata= new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), userProfileId);
+        String fileName = String.format("%s-%s", file.getOriginalFilename(), UUID.randomUUID());
+
+        try {
+            fileStore.save(path, fileName, Optional.of(metadata), file.getInputStream());
+            user.setUserProfileImageLink(fileName);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
 
     }
 }
