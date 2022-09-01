b81f91f88984f5613bd1444d0b9789ab4db04a71
resuluygun <resuluygunn@gmail.com>
2022-09-01 23:26:24 +0300

    add missing anotation for FileStore

--- src/main/java/com/resbus/demo/datastore/FakeUserProfileDataStore.java
+++ src/main/java/com/resbus/demo/datastore/FakeUserProfileDataStore.java
@@ -12,8 +12,8 @@ public class FakeUserProfileDataStore {
     private  static final List<UserProfile> USER_PROFILES= new ArrayList<>();
 
     static {
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(),"res", null));
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(),"cerenc", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("a9e512d0-b94a-4c65-9019-f409a123498d"),"res", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("a4d3e971-3a6e-4b62-915a-81f6aeca6ce9"),"cerenc", null));
     }
 
 
--- src/main/java/com/resbus/demo/filestore/FileStore.java
+++ src/main/java/com/resbus/demo/filestore/FileStore.java
@@ -4,13 +4,14 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 import java.util.Map;
 import java.util.Optional;
 
+@Service
 public class FileStore {
-
     private final AmazonS3 s3;
 
     @Autowired // for dependency injection
